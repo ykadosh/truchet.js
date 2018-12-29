f95b9fbef824dd8344c5ee13d5bff330ead7bb02
Yoav Kadosh <yoavks@gmail.com>
2018-12-29 23:58:45 +0200

    Added more icons to footer

--- docs/src/components/Footer/Footer.jsx
+++ docs/src/components/Footer/Footer.jsx
@@ -7,9 +7,11 @@ export default class Footer extends React.PureComponent {
         return (
             <footer>
                 <div className='user-actions'>
-                    <a href=''><Icon type={Icon.Types.GITHUB}/>Source</a>
-                    <a href=''><Icon type={Icon.Types.CODEPEN}/>Pens</a>
-                    <a href={`https://twitter.com/intent/tweet?text=${encodeURIComponent('truchet.js is incredible!')}&hashtags=truchetjs&url=https://github.com/`}><Icon type={Icon.Types.TWITTER}/>Share</a>
+                    <a href='https://github.com/ykadosh/truchet.js'><Icon type={Icon.Types.GITHUB}/>Source</a>
+                    <a href='https://codepen.io/collection/nYQyZY/'><Icon type={Icon.Types.CODEPEN}/>Pens</a>
+                    <a href={`https://twitter.com/intent/tweet?text=${encodeURIComponent('Create beautiful patterns with truchet.js')}&hashtags=truchetjs&url=https://github.com/ykadosh/truchet.js`}><Icon type={Icon.Types.TWITTER}/>Share</a>
+                    <a href='https://www.npmjs.com/package/truchet'><Icon type={Icon.Types.NPM}/>Package</a>
+                    <a href='https://unpkg.com/truchet'><Icon type={Icon.Types.UNPKG}/>UNPKG</a>
                 </div>
                 <div className='credits'>Developed with ☕ by Yoav Kadosh</div>
             </footer>
