4fb7148155b6262ac75033cd8c9dc458a010c783
Yoav Kadosh <yoavks@gmail.com>
2019-02-11 00:02:40 +0200

    Updated docs

--- docs/src/components/Header/Header.jsx
+++ docs/src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import GithubCorner from '../GithubCorner/GithubCorner';
-import Logo from '../Logo/Logo';
 import './Header.scss';
 
 export default class Header extends React.PureComponent {
